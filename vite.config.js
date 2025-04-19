9f78ad6b065ae36a1396df804972be3f4ba6316a
Zax71 <67716263+zax71@users.noreply.github.com>
2025-04-19 16:58:27 +0100

    Add Tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
+// NOTE: there is a TS Error with the addition of the Tailwind plugin, but it still works correctly when run.
 export default defineConfig(async () => ({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), tailwindcss()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
